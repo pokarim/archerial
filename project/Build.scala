7d351d305bc72a815ff7b62a5a99f1e747025699
pokarim <pokarim@gmail.com>
2012-10-10 17:02:26 +0900

    add dependency to Jerkson

--- project/Build.scala
+++ project/Build.scala
@@ -10,18 +10,24 @@ object ArcherialBuild extends Build {
 	  "org.scalaz" %% "scalaz-core" % "6.0.4",
 	  "play" %% "play" % "2.0.4",
 	  "play" %% "play" % "2.0.4" % "test",
-	  "org.specs2" %% "specs2" % "1.12.1" % "test"
+	  "org.specs2" %% "specs2" % "1.12.1" % "test",
+	  "com.codahale" %% "jerkson" % "0.5.0"
     )
   val mysettings = Project.defaultSettings ++ Seq(
 	libraryDependencies ++= appDependencies,
 	resolvers ++= Seq(
-    DefaultMavenRepository,
+      DefaultMavenRepository,
+	  "Codahale" at "http://repo.codahale.com",
 	  "Typesafe repo" at "http://repo.typesafe.com/typesafe/repo",
-	  "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-      "releases"  at "http://oss.sonatype.org/content/repositories/releases",
-      Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns),
-      Resolver.url("Typesafe Repository", 
-				   url("http://repo.typesafe.com/typesafe/releases/"))(Resolver.ivyStylePatterns)))
+	  "snapshots" at 
+	  "http://oss.sonatype.org/content/repositories/snapshots",
+      "releases"  at 
+	  "http://oss.sonatype.org/content/repositories/releases",
+      Resolver.url(
+		"Play", 
+		url("http://download.playframework.org/ivy-releases/")
+	  )(Resolver.ivyStylePatterns)
+	))
   lazy val root = Project(id = "Archerial",
                           base = file("."),
                           settings = mysettings)
